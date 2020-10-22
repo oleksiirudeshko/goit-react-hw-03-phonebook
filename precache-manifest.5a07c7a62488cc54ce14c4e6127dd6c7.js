a1a3125cdd539399530703ee2cc9eb391286e819
Oleksii <clothes.lyks@gmail.com>
2020-10-22 14:32:53 +0300

    Updates

---
+++ precache-manifest.5a07c7a62488cc54ce14c4e6127dd6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3933e0cab4d7466be654a5f3ff643f8",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "6a6149d1418b59e31b2b",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.62f5930b.chunk.css"
+  },
+  {
+    "revision": "aabf239b83e0fea632fa",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.8e244730.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.8e244730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6149d1418b59e31b2b",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.fe349745.chunk.js"
+  },
+  {
+    "revision": "356906f07e3d724d1528",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.d720b385.js"
+  }
+]);
